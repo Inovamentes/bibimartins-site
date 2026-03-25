a74c1a2feb3e12989b3a7d96fc7eaeabaf97a095
Inovamentes <inovamentes.bm@gmail.com>
2026-03-25 20:25:19 -0300

    Updated logo from 'B' to 'BM' in header, mobile menu, and footer sections.

---
+++ App.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <header>
+        <h1>BM</h1>
+      </header>
+      <nav>
+        <ul>
+          <li>BM</li>
+        </ul>
+      </nav>
+      <footer>
+        <p>BM</p>
+      </footer>
+    </div>
+  );
+};
+
+export default App;
