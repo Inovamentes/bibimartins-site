58f8a517f86402a6a822a3e239a42997f88e49e5
Inovamentes <inovamentes.bm@gmail.com>
2026-04-06 14:35:30 -0300

    Add package declaration and imports in SecurityConfig

--- backend/src/main/java/com/bibimartins/config/SecurityConfig.java
+++ backend/src/main/java/com/bibimartins/config/SecurityConfig.java
@@ -1,1 +1,6 @@
+package com.bibimartins.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
