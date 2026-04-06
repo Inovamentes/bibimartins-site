f3f041e023018bd656959fc31a2ffa6a7d07acb9
Inovamentes <inovamentes.bm@gmail.com>
2026-04-06 14:19:56 -0300

    Initialize Spring Boot application structure

---
+++ backend/src/main/java/com/bibimartins/BibimartinsApplication.java
@@ -0,0 +1,8 @@
+package com.bibimartins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibimartinsApplication {
+    public static void main(String
